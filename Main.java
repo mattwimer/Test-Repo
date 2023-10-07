710eaf36406d783c5002c7737019e137083e901a
Matt Wimer (Home) <mattwimer1@gmail.com>
2023-10-07 14:54:17 -0700

    Added some user interaction.

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 class Main{
     public static void main(String[] args) {
         System.out.println("Hello Anastasia!");
+        Scanner in = new Scanner(System.in);
+        System.out.println("How are you today? ");
+        in.nextLine();
     }
 }
